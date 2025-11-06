09d96b699a3a3f3d5b6fe03901195bdcd0660956
VadimSodomov <vadim.sodomov@yandex.ru>
2025-11-06 22:28:07 +0300

    Поправил миграцию

--- backend/migrations/Version20251103220440.php
+++ backend/migrations/Version20251103220440.php
@@ -16,16 +16,15 @@ final class Version20251103220440 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE file (id BIGSERIAL NOT NULL, user_id BIGINT NOT NULL, folder_id BIGINT NOT NULL, name TEXT NOT NULL, server_name TEXT NOT NULL, cdate TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, mime_type TEXT NOT NULL, size BIGINT NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX IDX_8C9F3610A76ED395 ON file (user_id)');
-        $this->addSql('CREATE INDEX IDX_8C9F3610162CB942 ON file (folder_id)');
-        $this->addSql('ALTER TABLE file ADD CONSTRAINT FK_8C9F3610A76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE file ADD CONSTRAINT FK_8C9F3610162CB942 FOREIGN KEY (folder_id) REFERENCES folder (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE TABLE IF NOT EXISTS file (id BIGSERIAL NOT NULL, user_id BIGINT NOT NULL, folder_id BIGINT NOT NULL, name TEXT NOT NULL, server_name TEXT NOT NULL, cdate TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, mime_type TEXT NOT NULL, size BIGINT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IF NOT EXISTS IDX_8C9F3610A76ED395 ON file (user_id)');
+        $this->addSql('CREATE INDEX IF NOT EXISTS IDX_8C9F3610162CB942 ON file (folder_id)');
+        $this->addSql('ALTER TABLE file ADD CONSTRAINT FK_8C9F3610A76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE file ADD CONSTRAINT FK_8C9F3610162CB942 FOREIGN KEY (folder_id) REFERENCES folder (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE file DROP CONSTRAINT FK_8C9F3610A76ED395');
         $this->addSql('ALTER TABLE file DROP CONSTRAINT FK_8C9F3610162CB942');
         $this->addSql('DROP TABLE file');
