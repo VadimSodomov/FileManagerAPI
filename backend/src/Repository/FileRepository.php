4e5ec6b7ab86816873a79b095c2e195990c5a031
VadimSodomov <vadim.sodomov@yandex.ru>
2025-11-06 22:30:24 +0300

    Рекурсивный проход по дереву папок
    Summary: все файлы, необходимые для удаления папки

--- backend/src/Repository/FileRepository.php
+++ backend/src/Repository/FileRepository.php
@@ -16,6 +16,32 @@ class FileRepository extends ServiceEntityRepository
         parent::__construct($registry, File::class);
     }
 
+    public function getFilesForDelete(int $folderId): array|false
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = <<<SQL
+        WITH RECURSIVE folder_tree AS (
+            SELECT id
+            FROM folder
+            WHERE id = :folder_id
+        
+            UNION ALL
+        
+            SELECT f.id
+            FROM folder f
+                     INNER JOIN folder_tree ft ON f.parent_id = ft.id
+        )
+        SELECT f.server_name
+        FROM file f
+                 INNER JOIN folder_tree ft ON f.folder_id = ft.id;
+        SQL;
+
+        $result = $conn->executeQuery($sql, ['folder_id' => $folderId])->fetchAllAssociative();
+
+        return array_column($result, 'server_name');
+    }
+
     //    /**
     //     * @return File[] Returns an array of File objects
     //     */
